b5ebff60d83d75d2bb5b3cc63a7ec3bfbdbd6dfa
asafiniu <asafiniu@gmail.com>
2013-05-19 18:02:03 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projectsunshine::Application.config.secret_token = 'ad776febd2fa8536d4884f3680e2feec35727b1d565611016654a8ad5dc5c874ef273219f2f03dd042c9b9cf49aaa5f7756fb1fdf42f7a983ea937af190b173c'
